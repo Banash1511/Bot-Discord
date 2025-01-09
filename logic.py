8cd77c4745e0b05faeb4a1fa417dc81d3ed9be23
Banash1511 <banash1511@gmail.com>
2025-01-08 21:36:56 -0500

    Primer envio

---
+++ logic.py
@@ -0,0 +1,20 @@
+import random as r, string as s
+
+def contra(largo):
+    elements = s.ascii_letters+s.ascii_lowercase+s.ascii_uppercase+s.digits+s.punctuation
+    password = ""
+
+    for i in range(largo):
+        password += r.choice(elements)
+    return password
+
+def gen_emoji():
+    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return r.choice(emoji)
+
+def moneda():
+    flip = r.randint(0, 2)
+    if flip == 0:
+        return "Cara"
+    else:
+        return "Cruz"
